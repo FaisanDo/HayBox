c1b1e6055902ba3a2e704588f80cf62a70efa26b
FaisanDo <daventorres.00@gmail.com>
2026-06-19 21:15:16 -0600

    Update FgcMode.cpp
    No duplicate directional buttons (Up) per Capcom rules.

--- src/modes/FgcMode.cpp
+++ src/modes/FgcMode.cpp
@@ -8,9 +8,9 @@ FgcMode::FgcMode(socd::SocdType horizontal_socd, socd::SocdType vertical_socd) {
   properly if Down and both Up buttons are pressed, because it first resolves Down + Mod X
   to set both as unpressed, and then it sees C-Up as pressed but not Down, so you get an up
   input instead of neutral. */
-        socd::SocdPair{ &InputState::mod_x, &InputState::c_up,  socd::SOCD_DIR1_PRIORITY},
+        //socd::SocdPair{ &InputState::mod_x, &InputState::c_up,  socd::SOCD_DIR1_PRIORITY},
         socd::SocdPair{ &InputState::down,  &InputState::mod_x, vertical_socd           },
-        socd::SocdPair{ &InputState::down,  &InputState::c_up,  vertical_socd           },
+        // socd::SocdPair{ &InputState::down,  &InputState::c_up,  vertical_socd           },
     };
 }
 
@@ -21,12 +21,15 @@ void FgcMode::UpdateDigitalOutputs(InputState &inputs, OutputState &outputs) {
     outputs.dpadLeft = inputs.left;
     outputs.dpadRight = inputs.right;
     outputs.dpadDown = inputs.down;
-    outputs.dpadUp = inputs.mod_x || inputs.c_up;
+    outputs.dpadUp = inputs.mod_x;
+    // outputs.dpadUp = inputs.mod_x || inputs.c_;
 
     // Menu keys
     outputs.start = inputs.start;
     outputs.select = inputs.c_left;
     outputs.home = inputs.c_down;
+    outputs.leftStickClick = inputs.mod_y;
+    outputs.rightStickClick = inputs.a;
 
     // Right hand bottom row
     outputs.a = inputs.b;
